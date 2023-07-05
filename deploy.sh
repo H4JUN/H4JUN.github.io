4920d89f214d86222019f552fc3233350794023d
tyson <ihazun01@gmail.com>
2023-07-04 23:06:23 -0400

    Update commit message

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 # Do I need to delete thebranch?
 sudo trunk build --release
-git add ./ && git commit -m "Update dist"
+git add ./ && git commit -m "Update gh-pages"
 git subtree push --prefix dist origin gh-pages
 
